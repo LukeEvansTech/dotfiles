0cbd094c3e4309a899ef7d336383d47565e8acd1
Luke Evans <contactluke@gmail.com>
2024-02-23 09:39:07 +0000

    Update ssh-add command to use absolute path

--- ssh.sh
+++ ssh.sh
@@ -15,7 +15,7 @@ echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed2
 
 # It appears that there is another ssh-add, possibly installed by Brew, in your shell's $PATH that differs from the Apple version. When using the Apple version,  -K  saves your password in the keychain, eliminating the need to type it repeatedly. However, the non-Apple version  -K  loads resident keys from a FIDO authenticator.
 # ssh-add -K ~/.ssh/id_ed25519
-ssh-add --apple-use-keychain ~/.ssh/id_ed25519 
+/usr/bin/ssh-add --apple-use-keychain ~/.ssh/id_ed25519 
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
