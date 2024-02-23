fff542d6c7d3cd0839dbc06b32e9fe8fdae8f818
Luke Evans <contactluke@gmail.com>
2024-02-23 10:11:51 +0000

    Update ssh.sh script to use absolute paths for ssh-keygen and ssh-agent

--- ssh.sh
+++ ssh.sh
@@ -4,14 +4,14 @@ echo "Generating a new SSH key for GitHub..."
 
 # Generating a new SSH key
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
-ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
+/usr/bin/ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
-eval "$(ssh-agent -s)"
+eval "$(/usr/bin/ssh-agent -s)"
 
 touch ~/.ssh/config
-echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+echo "Host *\n IgnoreUnknown UseKeychain\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
 
 # It appears that there is another ssh-add, possibly installed by Brew, in your shell's $PATH that differs from the Apple version. When using the Apple version,  -K  saves your password in the keychain, eliminating the need to type it repeatedly. However, the non-Apple version  -K  loads resident keys from a FIDO authenticator.
 # ssh-add -K ~/.ssh/id_ed25519
